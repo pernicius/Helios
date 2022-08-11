e61a112e4e9310b0e7b1f2d314c21b745c61a07a
Pernicius <pernicius@web.de>
2022-08-11 23:04:49 +0200

    force ImGui to use the glad loader and not his own

--- projects.client/HeliosEngine/source/HeliosEngine/ImGui/ImGuiBuild.cpp
+++ projects.client/HeliosEngine/source/HeliosEngine/ImGui/ImGuiBuild.cpp
@@ -1,7 +1,13 @@
 #include "pch_engine.h"
 
+#include "glad/gl.h"
+
+// force ImGui to use the glad loader
+#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+
+// disable MSVC's warning about deprecated/unsafe code
 #pragma warning(disable : 4996)
 
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+// TODO: needs to be switchable on runtime (like Platform/Renderer/...)
 #include "backends/imgui_impl_opengl3.cpp"
 #include "backends/imgui_impl_glfw.cpp"
